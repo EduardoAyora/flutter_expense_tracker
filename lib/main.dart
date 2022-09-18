410c8bb7bce7fc1d1cdcb54df4896889a214c5e5
EduardoAyora <eduardoaayora64@gmail.com>
2022-09-17 22:11:35 -0500

    Agregar boton para ingresar tareas

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import './widgets/user_transactions.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gastos Personales',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gastos Personales'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Card(
+              color: Colors.blue,
+              child: Container(
+                width: double.infinity,
+                child: const Text('Chart!'),
+              ),
+              elevation: 5,
+            ),
+            UserTransactions(),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (() {}),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/user_transactions.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import './transaction_list.dart';
+import './new_transaction.dart';
+import '../models/transaction.dart';
+
+class UserTransactions extends StatefulWidget {
+  const UserTransactions({Key key}) : super(key: key);
+
+  @override
+  State<UserTransactions> createState() => _UserTransactionsState();
+}
+
+class _UserTransactionsState extends State<UserTransactions> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      amount: 2.99,
+      title: 'New shoes',
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      amount: 13.99,
+      title: 'Compras',
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String title, double amount) {
+    final newTx = Transaction(
+      id: DateTime.now().toString(),
+      amount: amount,
+      date: DateTime.now(),
+      title: title,
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        NewTransaction(_addNewTransaction),
+        TransactionList(_userTransactions),
+      ],
+    );
+  }
+}
